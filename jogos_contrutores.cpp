39f7ba41a6be9b69400faa622a6073cc6d3c672d
parasaiteve <101370780+parasaiteve@users.noreply.github.com>
2023-05-06 14:58:37 -0300

    update

--- jogos_contrutores.cpp
+++ jogos_contrutores.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-// criacao da classe
+/* criacao da classe
 class JogoAdivinhacao {
     
     private:
@@ -16,24 +16,24 @@ class JogoAdivinhacao {
          NumeroSecreto = rand() % 100 + 1;
          Tentativas = 0;
         }
-    // usar o metodo para iniciar o jogo
+    /* usar o metodo para iniciar o jogo
     void Jogar() {
-        // usar a condicao do, while e if juntas para poder funcionar dentro das condicoes
+        /* usar a condicao do, while e if juntas para poder funcionar dentro das condicoes
         int palpite;
         do {
-          // interface de usuario
+          /* interface de usuario
           cout << "Digite um numero entre 1 e 100 >>> ";
-          // leitura do numero chutado
+          /* leitura do numero chutado
           cin >> palpite;
           Tentativas++;
-          // caso o numero chutado seje maior que o numero sorteado
+          /* caso o numero chutado seje maior que o numero sorteado
           if (palpite > NumeroSecreto) { 
               cout << "o numero secreto e menor!" << endl;
           }else if (palpite < NumeroSecreto) {
-              // caso o numero chutado seje maior que o numero sorteado
+              /* caso o numero chutado seje maior que o numero sorteado
               cout << "o numero secreto e maior!" << endl;
           }else {
-              // caso o usuario acerte o numero sorteado e o numeros de tentativas 
+              /* caso o usuario acerte o numero sorteado e o numeros de tentativas 
               cout << "Parabens, voce acertou o numero em " << Tentativas << " tentativas!" << endl;
           }
           
@@ -41,11 +41,11 @@ class JogoAdivinhacao {
     }    
      
 };
-// declaracao da variavel
+/* declaracao da variavel
 int main () {
     JogoAdivinhacao jogo;
-    // construtor com o mesmo nome do metodo chamando a acao na linha 20
+    /* construtor com o mesmo nome do metodo chamando a acao na linha 20
     jogo.Jogar();
-    // fechamento do programa com um retorno
+    /* fechamento do programa com um retorno
     return 0;
 }
